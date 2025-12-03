d37b1e84c2290b31caf2479d879a2fc305f37cdc
AntoniaObersteiner <antonia.obersteiner@gmail.com>
2025-12-03 13:08:54 +0100

    checkout the experimental branch after init+update

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # not recursive, otherwise errors may occur
-git submodule update --init --branch experimental
+git submodule update --init
 
 # patch submodules urls (ssh-address -> https-address)
 cd modules/SIMDOps
@@ -10,6 +10,7 @@ cp ../../simdops_modules_patch .gitmodules
 # sync urls and update modules
 git submodule sync
 git submodule update --init
+git checkout experimental
 
 # generate tsl with the availaber vector extensions
 # python3 tools/tslgen/main.py --targets sse sse2 sse3 ssse3 sse4.1 sse4.2 avx avx2 avx512 --no-workaround-warnings -o ../../libs/tsl
